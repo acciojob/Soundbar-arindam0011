5ab6b199b178b2d936a978c65a483f0c4799032d
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-20 21:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,32 +8,32 @@ const wrong=document.getElementById("wrong");
 const stop=document.getElementById("stop");
  let sound=[];
 applause.addEventListener("click", function(){
-	let mp3=new Audio('https://drive.google.com/drive/folders/1UbicLbJlF28MO6Yw4zZDhQjGbnXZcodt');
+	let mp3=new Audio('https://od.lk/s/MTFfMzg5MTU3NzVf/applause-180037.mp3');
 	mp3.play();
 	sound.push(mp3);
 });
 boo.addEventListener("click", function(){
-	let mp3=new Audio('https://drive.google.com/drive/folders/1UbicLbJlF28MO6Yw4zZDhQjGbnXZcodt');
+	let mp3=new Audio('https://od.lk/s/MTFfMzg5MTU3NThf/boo-6377.mp3');
 	mp3.play();
 	sound.push(mp3);
 });
 gasp.addEventListener("click", function(){
-	let mp3=new Audio('https://drive.google.com/drive/folders/1UbicLbJlF28MO6Yw4zZDhQjGbnXZcodt');
+	let mp3=new Audio('https://od.lk/s/MTFfMzg5MTU3NTRf/gasp-7117.mp3');
 	mp3.play();
 	sound.push(mp3);
 });
 tada.addEventListener("click", function(){
-	let mp3=new Audio('https://drive.google.com/drive/folders/1UbicLbJlF28MO6Yw4zZDhQjGbnXZcodt');
+	let mp3=new Audio('https://od.lk/s/MTFfMzg5MTU3NTFf/tada-fanfare-a-6313.mp3');
 	mp3.play();
 	sound.push(mp3);
 });
 victory.addEventListener("click", function(){
-	let mp3=new Audio('https://drive.google.com/drive/folders/1UbicLbJlF28MO6Yw4zZDhQjGbnXZcodt');
+	let mp3=new Audio('https://od.lk/s/MTFfMzg5MTU3NDhf/victory-1-90174.mp3');
 	mp3.play();
 	sound.push(mp3);
 });
 wrong.addEventListener("click", function(){
-	let mp3=new Audio('https://drive.google.com/drive/folders/1UbicLbJlF28MO6Yw4zZDhQjGbnXZcodt');
+	let mp3=new Audio('https://od.lk/s/MTFfMzg5MTU3ODBf/buzzer-or-wrong-answer-20582.mp3');
 	mp3.play();
 	sound.push(mp3);
 });
@@ -41,8 +41,8 @@ wrong.addEventListener("click", function(){
 stop.addEventListener("click", function(){
 
 	sound.forEach(snd=>{
-		snd.push();
+		snd.pause();
 		snd.currentTime = 0;
 	})
-	sounds = []; 
+	sound = []; 
 });
